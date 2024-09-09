d2ef5211ab056e725e3a33502c0bf01a4bee1768
seszele64 <sebastian@szelewi.cz>
2024-09-09 16:00:54 +0200

    - move user agent to its own folder - create init to make module

--- user_agent/user_agent.py
+++ user_agent/user_agent.py
@@ -1,11 +1,18 @@
 from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
+from random_user_agent.params import SoftwareName, OperatingSystem, Popularity
 
 class UserAgentGenerator:
     def __init__(self):
         software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.EDGE.value]
         operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MAC.value]
-        self.user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+        popularity = [Popularity.POPULAR.value, Popularity.COMMON.value]
+
+        self.user_agent_rotator = UserAgent(
+            software_names=software_names,
+            operating_systems=operating_systems,
+            popularity=popularity,
+            limit=100
+        )
 
     def get_random_user_agent(self):
         return self.user_agent_rotator.get_random_user_agent()
