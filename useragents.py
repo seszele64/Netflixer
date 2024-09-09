ac58bef22a15f0547f633c20f029252fd0cde562
seszele64 <sebastian@szelewi.cz>
2024-09-09 15:34:51 +0200

    feat: Add UserAgentGenerator class for generating random user agents

---
+++ useragents.py
@@ -0,0 +1,14 @@
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+
+class UserAgentGenerator:
+    def __init__(self):
+        software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.EDGE.value]
+        operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MAC.value]
+        self.user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+
+    def get_random_user_agent(self):
+        return self.user_agent_rotator.get_random_user_agent()
+
+    def get_user_agents(self):
+        return self.user_agent_rotator.get_user_agents()
